910af07eff88d27308033a283ea2401fbc40e477
Fabio Marsiaj <fabiobackend@gmail.com>
2024-01-04 00:25:58 -0300

    better way of IoC with interface

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+interface Repository {
+  findOne(id: string);
+  findAll();
+  create(content: string);
+}
+
 @Injectable()
 export class MessagesService {
-  constructor(private repository: MessagesRepository) {}
+  messageRepo: Repository;
+
+  constructor(repo: Repository) {
+    this.messageRepo = repo;
+  }
 
   findOne(id: string) {
-    return this.repository.findOne(id);
+    return this.messageRepo.findOne(id);
   }
 
   findAll() {
-    return this.repository.findAll();
+    return this.messageRepo.findAll();
   }
 
   create(content: string) {
-    return this.repository.create(content);
+    return this.messageRepo.create(content);
   }
 }
